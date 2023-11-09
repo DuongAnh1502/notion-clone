a09491a5096484d1a173e3dab7a2c015c4e31ac1
duonganh203 <rankill52@gmail.com>
2023-11-09 19:51:33 +0700

    darkmode

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -7,15 +7,27 @@ export const Heroes = () => {
                     <Image
                         src='/documents.png'
                         fill
-                        className='object-contain'
+                        className='object-contain dark:hidden'
                         alt='documents'
                     />
+                    <Image
+                        src='/documents-dark.png'
+                        fill
+                        className='object-contain hidden dark:block'
+                        alt='reading'
+                    ></Image>
                 </div>
                 <div className='relative h-[400px] w-[400px] hidden md:block'>
                     <Image
                         src='/reading.png'
                         fill
-                        className='object-contain'
+                        className='object-contain dark:hidden'
+                        alt='reading'
+                    ></Image>
+                    <Image
+                        src='/reading-dark.png'
+                        fill
+                        className='object-contain hidden dark:block'
                         alt='reading'
                     ></Image>
                 </div>
