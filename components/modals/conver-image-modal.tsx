deee0a16ad346aa2a9f353b709d1f659a76368ad
duonganh203 <rankill52@gmail.com>
2023-12-06 20:25:52 +0700

    remove cover in databasse

--- components/modals/conver-image-modal.tsx
+++ components/modals/conver-image-modal.tsx
@@ -25,7 +25,10 @@ export const CoverImageModal = () => {
         if (file) {
             setIsSubmitting(true);
             setFile(file);
-            const res = await edgestore.publicFiles.upload({ file });
+            const res = await edgestore.publicFiles.upload({
+                file,
+                options: { replaceTargetUrl: coverImage.url },
+            });
             await update({
                 id: params.documentId as Id<"documents">,
                 coverImage: res.url,
